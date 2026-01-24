77b017a7751060c33783a09c5d80e6c29110e9d7
Parth Srivastav <parthsrivastav.00@gmail.com>
2026-01-25 00:35:38 +0530

    test(pool): add unit tests for pool initialization and task queue handling

---
+++ pool_test.go
@@ -0,0 +1,36 @@
+package jobq
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewPoolWithQueueTask(t *testing.T) {
+	totalN := int64(5)
+	taskN := 10
+	rets := make(chan struct{}, taskN)
+
+	p := NewPool(totalN)
+	for i := 0; i < taskN; i++ {
+		assert.NoError(t, p.EnQueueTask(func(context.Context) error {
+			rets <- struct{}{}
+			return nil
+		}))
+	}
+	for i := 0; i < taskN; i++ {
+		<-rets
+	}
+	// shutdown all, and now running worker is 0
+	p.Release()
+	assert.Equal(t, int64(0), p.BusyWorkers())
+}
+
+func TestPoolNumber(t *testing.T) {
+	p := NewPool(0)
+	p.Start()
+	// shutdown all, and now running worker is 0
+	p.Release()
+	assert.Equal(t, int64(0), p.BusyWorkers())
+}
